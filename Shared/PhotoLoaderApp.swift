6e09d19cb08ba62a3175e95ebe3dd6bbade752fe
Alexander von Below <Alex@vonBelow.Com>
2021-11-17 12:29:44 +0100

    Initial Commit

---
+++ Shared/PhotoLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLoaderApp.swift
+//  Shared
+//
+//  Created by Alexander v. Below on 17.11.21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
